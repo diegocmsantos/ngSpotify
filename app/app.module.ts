af58a2f7086c5c2a0cfe620657e153a3679e1ec9
Diego Maia <diegocmsantos@gmail.com>
2016-12-26 15:54:19 -0300

    Preparing sstructure

---
+++ app/app.module.ts
@@ -0,0 +1,19 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { AboutComponent } from './components/about/about.component';
+
+import { routing } from './app.routing';
+
+@NgModule({
+  imports:      [ BrowserModule, routing ],
+  declarations: [ AppComponent,
+                  NavbarComponent,
+                  SearchComponent,
+                  AboutComponent ],
+  bootstrap:    [ AppComponent ]
+})
+export class AppModule { }
