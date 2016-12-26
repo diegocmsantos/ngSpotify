8643a285c5ad02faeae57f62d554bb7cb11c3953
Diego Maia <diegocmsantos@gmail.com>
2016-12-26 16:48:58 -0300

    creating search page

--- app/components/search/search.component.ts
+++ app/components/search/search.component.ts
@@ -1,8 +1,27 @@
 import { Component } from '@angular/core';
+import { SpotifyService } from '../../services/spotify.service'
+import { Artist } from '../../models/Artist';
 
 @Component({
   moduleId: module.id,
   selector: 'search',
   templateUrl: 'search.component.html',
 })
-export class SearchComponent  {}
+export class SearchComponent  {
+
+  private searchStr:string;
+  private _artists: Artist[];
+
+  constructor(private _spotifyService: SpotifyService) {
+
+  }
+
+  searchMusic() {
+    this._spotifyService.searchMusic(this.searchStr)
+      .subscribe(res => {
+        console.log(res);
+        this._artists = res.artists.items;
+      });
+  }
+
+}
