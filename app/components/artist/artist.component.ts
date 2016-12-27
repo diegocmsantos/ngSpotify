f577880a062708e3dbbf49f9134c6677952e1643
Diego Maia <diegocmsantos@gmail.com>
2016-12-27 00:19:20 -0300

    creating albums page

--- app/components/artist/artist.component.ts
+++ app/components/artist/artist.component.ts
@@ -23,10 +23,8 @@ export class ArtistComponent implements OnInit {
   ngOnInit() {
     this._route.params.map(params => params['id'])
       .subscribe(id => {
-        this._spotifyService.getArtist(id)
-          .subscribe(artist => {
-            this.artist = artist;
-          })
+        this._spotifyService.getArtist(id).subscribe(artist => this.artist = artist);
+        this._spotifyService.getAlbums(id).subscribe(albums => this.albums = albums.items);
       });
   }
 
